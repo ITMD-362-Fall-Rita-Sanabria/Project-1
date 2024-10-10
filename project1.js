592775a9d1a84e148db37fe144bd3f6913b6f843
rsanabria11 <rsanabria@hawk.iit.edu>
2024-10-10 12:48:02 -0500

    getting values for signup

---
+++ project1.js
@@ -0,0 +1,8 @@
+const form = getElementById('signupform');
+form.addEventListener('submit', function(event) {
+
+	const firstname = document.getElementById('firstname').value;
+	const lastname = document.getElementById('lastname').value;
+	const email = document.getElementById('email').value;
+ form.reset();
+}
