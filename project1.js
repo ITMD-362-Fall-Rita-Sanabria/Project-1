8500293c8037bebb8d55748c426e548b182a9c88
rsanabria11 <rsanabria@hawk.iit.edu>
2024-10-31 21:24:42 -0500

    edits for final turn in

--- project1.js
+++ project1.js
@@ -1,8 +1,13 @@
-const form = getElementById('signupform');
-form.addEventListener('submit', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
+	const form = document.getElementById('signupform');
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
 
-	const firstname = document.getElementById('firstname').value;
-	const lastname = document.getElementById('lastname').value;
-	const email = document.getElementById('email').value;
- form.reset();
-}
+		const firstname = document.getElementById('firstname').value;
+		const lastname = document.getElementById('lastname').value;
+		const email = document.getElementById('email').value;
+
+		form.reset();
+		alert("Thank you for signing up!");
+	});
+}); 
